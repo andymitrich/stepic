a32f6b61a4caffadaf86f429d998e0d66c23480f
andymitrich <andymitrich@gmail.com>
2014-05-14 09:12:29 +0400

    Dijkstra

--- rmq.py
+++ rmq.py
@@ -14,17 +14,22 @@ class RMQ(object):
             while n != self.n:
                 numbers.append(d('Infinity'))
                 n += 1
-            
-        for i in range(0, self.n-1, 1):
-            numbers.insert(0, min(numbers[self.n-i-1], numbers[self.n-i-2]))
+        else: self.n = n
+        
+        temp = numbers[::-1]
 
-        numbers.insert(0, d('Infinity'))
-        self.e = numbers
-        self.size = len(numbers)
-        # print(self.e)
+        for i in range(0, 2*(self.n-1), 2):
+            temp.append(min(temp[i], temp[i+1]))
+
+        temp.append(d('Infinity'))
+        self.e = temp[::-1]
+        self.size = len(temp)
       
     def recover(self, idx):
-        pass
+        while idx > 1:
+            parent = idx//2
+            self.e[parent] = min(self.e[2*parent], self.e[2*parent+1])
+            idx = parent
   
     def min(self, left, right, begin = 1, end = None, idx = 1):
         if end is None: end = self.n
@@ -32,36 +37,29 @@ class RMQ(object):
         if left==begin and right==end:
             return self.e[idx]
 
-        if left >= begin and right <= end//2:
-            # left child
-            min(left, right, begin, end//2, 2*idx)
-        elif left > end//2 and right <= end:
-            min(left, right, end//2, end)
-        elif 1:
-            return idx+1
-        
-        
+        mid = (begin + end) // 2
+
+        if left >= begin and right <= mid:
+            return self.min(left, right, begin, mid, 2*idx)
+        elif left > mid and right <= end:
+            return self.min(left, right, mid+1, end, 2*idx+1)
+        else:
+            left_child = self.min(left, mid, begin, mid, 2*idx)
+            right_child = self.min(mid+1, right, mid+1, end, 2*idx+1)
+            return min(left_child, right_child)
   
     def set(self, origin, value):
-        return
-        for i in range(self.size//2, self.size-1):
-
-            if self.e[i] == origin:
-                self.e[i] = value
-                self.recover(i)
-        print(self.e)
+        idx = self.size//2+origin-1
+        self.e[idx] = value
+        self.recover(idx)
   
     
 if __name__ == '__main__':
 
-    f = open('source/rmq.txt', 'r')
+    f = open('source/rmq2.txt', 'r')
     n, m = map(int, f.readline().split())
     numbers = list(map(int, f.readline().split()))
-    # print(n, m)
     rmq = RMQ(numbers)
-
-    # print(numbers)
-
 
     for i in range(0, m):
         c, x, y = f.readline().split()
@@ -69,5 +67,4 @@ if __name__ == '__main__':
         if c == 'Min':
           print(rmq.min(int(x), int(y)))
         elif c == 'Set':
-          rmq.set(x, y)
-
+          rmq.set(int(x), int(y))
